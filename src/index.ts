1c1eb8e385b275260d0dea892d3694e0e757a6c6
Thebigbot <77632836+athebigbot@users.noreply.github.com>
2023-02-22 19:58:21 +0100

    feat: terminal colors!

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,61 @@
 import { execSync } from "child_process";
 import { writeFileSync, mkdirSync } from "fs";
 import gitignore from "./gitignore";
+import colors from "colors";
 
-execSync(`pnpm init -y`);
+console.log(colors.yellow("Generating project..."));
 
 // Add some dependencies
-execSync(`pnpm add -D typescript ts-node ts-node-dev`);
+
+writeFileSync(
+  "./package.json",
+  JSON.stringify(
+    {
+      name: "",
+      version: "1.0.0",
+      description: "",
+      main: "index.js",
+      scripts: {
+        dev: 'ts-node-dev',
+        build: "tsc"
+      },
+      keywords: [],
+      author: "",
+      license: "ISC",
+      devDependencies: {
+        "ts-node": "^10.9.1",
+        "ts-node-dev": "^2.0.0",
+        "typescript": "^4.9.5",
+      },
+    },
+    null,
+    2
+  )
+);
 
 // Create tsconfig.json, .gitignore, src/index.ts
+console.log(colors.yellow("Creating files..."));
 writeFileSync(
   "./tsconfig.json",
-  JSON.stringify({
-    compilerOptions: {
-      target: "es6",
-      module: "es6",
-      lib: ["dom", "es6"],
-      strict: true,
-      esModuleInterop: true,
-      forceConsistentCasingInFileNames: true,
-      moduleResolution: "node",
-      outDir: "./dist",
-      sourceMap: true,
+  JSON.stringify(
+    {
+      compilerOptions: {
+        target: "es6",
+        module: "es6",
+        lib: ["dom", "es6"],
+        strict: true,
+        esModuleInterop: true,
+        forceConsistentCasingInFileNames: true,
+        moduleResolution: "node",
+        outDir: "./dist",
+        sourceMap: true,
+      },
+      include: ["src/**/*"],
+      exclude: ["node_modules", "dist"],
     },
-    include: ["src/**/*"],
-    exclude: ["node_modules", "dist"],
-  }, null, 2)
+    null,
+    2
+  )
 );
 
 writeFileSync("./.gitignore", gitignore);
@@ -34,4 +65,7 @@ mkdirSync("./src");
 writeFileSync("./src/index.ts", "");
 
 // Install all dependencies
+console.log(colors.yellow("Installing dependencies..."));
 execSync("pnpm install");
+
+console.log(colors.green("Project generated!"));
